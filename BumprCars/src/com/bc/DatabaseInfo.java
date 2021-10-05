ad3e822c3cdd18e26bd95ddfd8349fb0a78223ed
Jackelyn Yii <54428986+jackelyny@users.noreply.github.com>
2021-10-05 02:58:54 -0500

    Update DatabaseInfo.java

--- BumprCars/src/com/bc/DatabaseInfo.java
+++ BumprCars/src/com/bc/DatabaseInfo.java
@@ -5,7 +5,7 @@ public class DatabaseInfo {
 	public static final String PARAMETERS = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
 	public static final String USERNAME = "jyii";
-	public static final String PASSWORD = "Gummylova5790";
+	public static final String PASSWORD = "********";
 	public static final String URL = "jdbc:mysql://cse.unl.edu/" + USERNAME + PARAMETERS;
 
 }
